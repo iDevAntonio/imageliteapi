7e0bdccf5b8a094e83f242e2791b4e3a9811a0a0
idevantonio <antoniorex2@gmail.com>
2025-10-14 09:31:38 -0300

    JwtFilter

---
+++ src/main/java/io/github/idevantonio/imageliteapi/config/filter/JwtFilter.java
@@ -0,0 +1,70 @@
+package io.github.idevantonio.imageliteapi.config.filter;
+
+import io.github.idevantonio.imageliteapi.application.jwt.InvalidTokenException;
+import io.github.idevantonio.imageliteapi.application.jwt.JwtService;
+import io.github.idevantonio.imageliteapi.domain.entity.User;
+import io.github.idevantonio.imageliteapi.domain.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = getToken(request);
+
+        if(token !=null){
+            try{
+                String email = jwtService.getEmailFromToken(token);
+                User user = userService.getByEmail(email);
+                setUserAsAuthenticated(user);
+            } catch (InvalidTokenException e) {
+                log.error("Invalid token: {}", e.getMessage());
+            } catch (Exception e){
+                log.error("Token validation failed: {}", e.getMessage());
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void setUserAsAuthenticated(User user){
+        UserDetails userDetails = org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails,"", userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private String getToken(HttpServletRequest request){
+       String authHeader = request.getHeader("Authorization");
+       if (authHeader != null){
+           String[] authHeaderParts = authHeader.split("");
+           if(authHeaderParts.length == 2){
+               return authHeaderParts[1];
+           }
+       }
+       return null;
+
+    }
+}
